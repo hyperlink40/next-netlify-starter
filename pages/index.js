00f474cec298620e59fc2143a8b2739bdd121eda
HYPER40 <hyperlink406@gmail.com>
2025-07-13 09:55:46 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to HYPERLINK CONNECTIONS!" />
+    <h1>Hyperlink Connetion</h>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
