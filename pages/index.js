03419d2add0c9d1788544fdb2124fe84655576bb
HYPER40 <hyperlink406@gmail.com>
2025-07-13 10:59:09 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image' // Add this import
 
 export default function Home() {
   return (
@@ -18,6 +19,18 @@ export default function Home() {
         <p ClassName="descpription">
           <h1"Hyperlink Best Connections"/>
        </p>
+  
+       {/* Example: Display your image */}
+        <Image
+          src="/images/logo.png" // Change to your image file name
+          alt="Logo"
+          width={200}
+          height={200}
+        />
+
+        {/* Or using a standard img tag */}
+        {/* <img src="/images/logo.png" alt="Logo" width="200" height="200" /> */}
+      </main>
       </main>
 
       <Footer />
